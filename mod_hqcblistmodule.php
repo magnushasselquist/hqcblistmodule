b902b437c1e2ee2a550c4a81852dee97e8fda81f
Magnus Hasselquist <magnus.hasselquist@gmail.com>
2016-04-01 14:53:02 +0200

    Update mod_hqcblistmodule.php

--- mod_hqcblistmodule.php
+++ mod_hqcblistmodule.php
@@ -1,6 +1,12 @@
 <?php
 
-// no direct access
+/**
+ * @copyright   Copyright (C) 2014. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Magnus Hasselquist <magnus.hasselquist@gmail.com> - http://mintekniskasida.blogspot.se/
+ */
+ 
+// No direct access
 defined('_JEXEC') or die;
 
 // Include the syndicate functions only once
