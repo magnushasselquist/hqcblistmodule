f75a54bfd80de0a803c0295db1dad7823f03043e
Magnus Hasselquist <magnus.hasselquist@gmail.com>
2016-04-01 14:54:52 +0200

    Update helper.php

--- helper.php
+++ helper.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * Magnus Hasselquist HQ CB List Module
- * helper.php
- **/
+ * @copyright   Copyright (C) 2014. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Magnus Hasselquist <magnus.hasselquist@gmail.com> - http://mintekniskasida.blogspot.se/
+ */
+ 
+// No direct access
+defined('_JEXEC') or die;
 
 function db_field_replace($before_str, $user_id) {
 	$db = JFactory::getDbo();
